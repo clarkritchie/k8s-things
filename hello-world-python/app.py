19dae48f2f4a9b3388f73634f56cca689c51d90a
Clark Ritchie <clarkritchie@gmail.com>
2024-05-26 17:25:15 -0700

    adds in an env var named FOO

--- hello-world-python/app.py
+++ hello-world-python/app.py
@@ -1,9 +1,11 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    FOO = os.getenv('FOO', "a value for FOO does not exist")
+    return f'Hello, World! The value of the env var FOO is {FOO}'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6000)
--- hello-world-python/run.sh
+++ hello-world-python/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker run --rm -p 9000:6000 clarkdritchie/hello-world-python:latest
+# docker run --rm -p 9000:6000 -e FOO=baz hello-world-python:latest
+docker run --rm -p 9000:6000 -e FOO=baz clarkdritchie/hello-world-python:latest
